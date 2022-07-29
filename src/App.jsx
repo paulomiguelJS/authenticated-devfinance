8d787ded04002070a838f63a72f24fe0cbbae0f5
paulomigueljs <paulomiguelblues@gmail.com>
2022-07-28 22:55:41 -0400

    ThemeProvider created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { ThemeProvider } from "styled-components";
 
-import { Title } from './styles.js'
+import { defaultTheme } from "./styles/themes/default.js";
+
+import { Title } from "./styles.js";
 
 function App() {
-
   return (
- <Title fontSize={40}>Test</Title>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Title fontSize={40}>Test</Title>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
