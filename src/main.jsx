573ec665c923f199d3084f4b577af75412778c05
paulomigueljs <paulomiguelblues@gmail.com>
2022-08-07 10:04:43 -0400

    MirageJS implemented, first data viewed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
 import App from "./App";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transacitons", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
