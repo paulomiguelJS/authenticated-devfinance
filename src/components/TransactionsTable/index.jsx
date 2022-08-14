402a7af68f78e45bdbb3f3c2a36c1870e1f72cc7
paulomigueljs <paulomiguelblues@gmail.com>
2022-08-13 23:15:52 -0400

    Listing transactions and seeds

--- src/components/TransactionsTable/index.jsx
+++ src/components/TransactionsTable/index.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState([]);
   useEffect(() => {
-    api.get("transactions").then((response) => console.log(response.data));
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -20,18 +23,16 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Web developed</td>
-            <td className="deposit">$300.00</td>
-            <td>Developement</td>
-            <td>02/20/2022</td>
-          </tr>
-          <tr>
-            <td>Web developed</td>
-            <td className="withdraw">$500.00</td>
-            <td>Developement</td>
-            <td>02/20/2022</td>
-          </tr>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Container>
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2022-08-13 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Rent",
+          type: "withdraw",
+          category: "Apartament",
+          amount: 2000,
+          createdAt: new Date("2022-08-10 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
